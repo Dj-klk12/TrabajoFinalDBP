40a0635fdf9b26086c35985925db20d092df82a2
Dj-klk12 <frank.calcina@ucsp.edu.pe>
2024-06-22 03:18:03 -0500

    Agregar archivos de proyecto.

---
+++ Ecommerce/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce.Views;
+
+
+namespace Ecommerce;
+
+public partial class App : Application
+{
+	public App(LoginPage loginPage)
+	{
+		InitializeComponent();
+
+        var logueado = Preferences.Get("logueado", string.Empty);
+        if (string.IsNullOrEmpty(logueado))
+		{
+			MainPage = loginPage;
+        }
+		else
+		{
+            MainPage = new AppShell();
+        }
+
+            
+	}
+}
